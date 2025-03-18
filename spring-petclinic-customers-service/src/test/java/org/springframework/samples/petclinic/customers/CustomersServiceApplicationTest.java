0580a11747c386949ca796da5ce53f3e479d148a
Quang Nguyen Huynh Minh <124662156+quang21122@users.noreply.github.com>
2025-03-18 11:43:25 +0700

    Update

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/CustomersServiceApplicationTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/CustomersServiceApplicationTest.java
@@ -2,8 +2,17 @@ package org.springframework.samples.petclinic.customers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+    "spring.cloud.discovery.enabled=false",
+    "spring.cloud.config.enabled=false",
+    "spring.cloud.config.discovery.enabled=false",
+    "spring.cloud.loadbalancer.enabled=false",
+    "eureka.client.enabled=false",
+    "spring.zipkin.enabled=false"
+})
 class CustomersServiceApplicationTest {
 
     @Test
@@ -14,6 +23,14 @@ class CustomersServiceApplicationTest {
     @Test
     void mainMethodStartsApplication() {
         // This test verifies that the main method can be called without throwing exceptions
-        CustomersServiceApplication.main(new String[]{});
+        CustomersServiceApplication.main(new String[]{
+            "--spring.cloud.discovery.enabled=false",
+            "--spring.cloud.config.enabled=false",
+            "--spring.cloud.config.discovery.enabled=false",
+            "--spring.cloud.loadbalancer.enabled=false",
+            "--eureka.client.enabled=false",
+            "--spring.zipkin.enabled=false",
+            "--server.port=0"
+        });
     }
 } 
