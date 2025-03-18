544b3a0c18b36f245dc2506dbc81ee4c8061f2cc
Quang Nguyen Huynh Minh <124662156+quang21122@users.noreply.github.com>
2025-03-18 11:19:10 +0700

    update

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/web/PetRequest.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/web/PetRequest.java
@@ -16,7 +16,6 @@
 package org.springframework.samples.petclinic.customers.web;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Date;
@@ -25,13 +24,10 @@ import java.util.Date;
  * @author mszarlinski@bravurasolutions.com on 2016-12-05.
  */
 record PetRequest(int id,
-                  @NotNull
                   @JsonFormat(pattern = "yyyy-MM-dd")
                   Date birthDate,
-                  @NotNull
                   @Size(min = 1)
                   String name,
-                  @NotNull
                   int typeId
 ) {
 
