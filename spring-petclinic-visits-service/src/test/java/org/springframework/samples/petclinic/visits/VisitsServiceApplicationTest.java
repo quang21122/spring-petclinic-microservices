10aac7baa629f2635e06e2f6ecbd88640524f387
Quang Nguyen Huynh Minh <124662156+quang21122@users.noreply.github.com>
2025-03-18 13:58:24 +0700

    update

--- spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/VisitsServiceApplicationTest.java
+++ spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/VisitsServiceApplicationTest.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @TestPropertySource(properties = {
@@ -17,5 +18,12 @@ class VisitsServiceApplicationTest {
     @Test
     void contextLoads(ApplicationContext context) {
         assertThat(context).isNotNull();
+    }
+
+    @Test
+    void mainMethodStartsApplication() {
+        assertDoesNotThrow(() -> {
+            VisitsServiceApplication.main(new String[]{});
+        });
     }
 }
